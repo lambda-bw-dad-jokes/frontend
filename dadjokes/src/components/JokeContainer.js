bd5423481d7c1ff9313915758fa771797b4eabed
Brenda Joshua <brendajosh.bj@gmail.com>
2019-10-21 22:21:52 +0300

    created jokecard,joke list,jokes and fetched data from the api

---
+++ dadjokes/src/components/JokeContainer.js
@@ -0,0 +1,23 @@
+//container
+const JokeContainer = () =>  {
+    const [myJoke, setJokeList] = useState([]);
+    const [update, setUpdate] = useState({});
+    const [isUpdating, setIsUpdating] = useState(false);
+
+    return(
+        <FlipDiv>
+            <Jokes
+                myJoke={myJoke}
+                setJokeList={setJokeList}
+                update={update}
+                isUpdating={isUpdating}
+            />
+          <JokeList 
+              myJoke={myJoke}
+              setUpdate={setUpdate}
+              setIsUpdating={setIsUpdating}
+          />  
+      </FlipDiv>
+      )
+    }
+    export default JokeContainer;
