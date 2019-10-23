2ce8f3cd9af36f110eec864efc681abd9c39fd4d
Greg Hayes <gthelight@gmail.com>
2019-10-23 00:47:20 -0500

    checking out to master

--- dadjokes/src/App.js
+++ dadjokes/src/App.js
@@ -1,53 +1,54 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Jokes from "./components/Jokes";
-import axios from 'axios'
-
-
+import axios from "axios";
 
 function App(props) {
-  const [data, setData] = useState([])
-  const [filteredData, setFilteredData] = useState([])
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
 
   useEffect(() => {
-    const getFeed= () => {
-            axios 
-                .get("http://jwhit-dadjokes.herokuapp.com/dadjokes/all?size=1000&page=0&sort=dadjokeid,desc")
+    const getFeed = () => {
+      axios
+        .get(
+          "http://jwhit-dadjokes.herokuapp.com/dadjokes/all?size=1000&page=0&sort=dadjokeid,desc"
+        )
 
-                .then(response => {
-                    console.log(response.data.jokes)
-                    setData(response.data.jokes)
-                })
+        .then(response => {
+          console.log(response.data.jokes);
+          setData(response.data.jokes);
+        })
 
-                .catch(error => {
-                console.log("Where are my jokes?", error)
-                });
-        }
+        .catch(error => {
+          console.log("Where are my jokes?", error);
+        });
+    };
 
-        getFeed();
-  }, [])
+    getFeed();
+  }, []);
 
-const searchJokesHandler = e => {
-  const jokes = data.filter(joke => {
-    if (joke.title.includes(e.target.value)) {
-      return joke
-    }
-  })
-  setFilteredData(jokes)
-}
+  const searchJokesHandler = e => {
+    const jokes = data.filter(joke => {
+      if (joke.title.includes(e.target.value)) {
+        return joke;
+      }
+    });
+    setFilteredData(jokes);
+  };
 
-  console.log('data', filteredData)
+  console.log("data", filteredData);
 
   return (
-        <>
-        <Router>
-            <Route path="/jokes" component={Jokes} />
-         </Router>
-        </>
-    <div className="App">
-      <h1> dad jokess </h1>
-    </div>
+    <>
+      <Router>
+        <Route path="/jokes" component={Jokes} />
+      </Router>
+
+      <div className="App">
+        <h1> dad jokess </h1>
+      </div>
+    </>
   );
 }
 
